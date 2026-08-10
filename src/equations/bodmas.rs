03af6bce1f41f9a7fe2e6fb2d63a29fda8e94478
ayoolaowoilu <ayoolaowoilu@gmail.com>
2026-08-10 23:37:16 +0100

    New : Created entry files

---
+++ src/equations/bodmas.rs
@@ -0,0 +1,9 @@
+
+
+
+pub fn calculate_bodmas(Equation:&str)-> f64{
+    let mut result:f64 = 0.0;
+    let mut elements:Vec<String> = map_elements(Equation);
+  
+    result
+}
---
+++ src/utils/element_seperator.rs
@@ -0,0 +1,11 @@
+
+
+
+pub fn element_identification(){
+  
+}
+
+pub fn map_elements(Equation:&str)-> Vec<String>{
+   
+}
+
